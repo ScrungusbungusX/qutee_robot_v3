4c192cc0156f9abae95ab1f42a6c4fd13028a771
Aedan Robertson <40653945@live.napier.ac.uk>
2026-04-20 17:51:26 +0100

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,5 @@
 #include "Qutee.hpp"
+#include "QuteeDxlPortHandler.hpp"
 #include "esp_log.h"
 
 static const char* TAG = "QUTEE_MAIN";
@@ -11,34 +12,13 @@ extern "C" void app_main() {
     robot.init();          // initialize motors, TFT, IMU, NVS
     //robot.select_name();   // choose or load robot name
     //robot.calibration();   // IMU calibration if required
-    robot.go_to_neutral_pose(); // reset motors
+    //robot.go_to_neutral_pose(); // reset motors
 
     ESP_LOGI(TAG, "Starting main control loop...");
 
     while (true) {
-        robot.run_episode();
-    }
-}
-
-#include "Qutee.hpp"
-#include "esp_log.h"
-
-static const char* TAG = "QUTEE_MAIN";
-
-extern "C" void app_main() {
-    ESP_LOGI(TAG, "Initializing Qutee...");
-
-    Qutee robot;
-
-    robot.init();          // initialize motors, TFT, IMU, NVS
-    robot.select_name();   // choose or load robot name
-    robot.calibration();   // IMU calibration if required
-    robot.go_to_neutral_pose(); // reset motors
-
-    ESP_LOGI(TAG, "Starting main control loop...");
-
-    while (true) {
-        robot.run_episode();
+        //robot.run_episode();
+        robot.test_move();
     }
 }
 
